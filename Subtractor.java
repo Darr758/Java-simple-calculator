511ee39eaaa1ba00646c3a6545a16a0e2e6e98c3
Darr758 <Darraghk500@yahoo.ie>
2015-09-09 22:52:42 +0100

    Create Subtractor.java

---
+++ Subtractor.java
@@ -0,0 +1,23 @@
+/**
+* This class implements the BinaryOperations interface and defines the apply method for Subtractor.
+*
+* @author Darragh King (ID 113372871)
+*/
+
+public class Subtractor implements BinaryOperations{
+	/**
+	*	Overrides apply to be used for Subtract.
+	*
+	*	@param firstOperand The first operand entered into the calculator by 
+	*			the user.
+	*	@param secondOperand The second operand entered into the calculator 
+	*			by the user.
+	*	@return A double representing the result of the requested computation.
+	*/
+
+  	@Override
+	public double apply(final double firstOperand,final double secondOperand){
+		double result = firstOperand - secondOperand;
+		return result;
+	}
+}
