05ef4b21d184dbc63e742d02148ab985399a4fd4
Darr758 <Darraghk500@yahoo.ie>
2015-09-09 22:47:39 +0100

    Create binaryOperations.java

---
+++ binaryOperations.java
@@ -0,0 +1,9 @@
+/**
+* An interface that declares the apply method of binary operations.
+*
+* @author Darragh King (ID 113372871)
+*/
+
+public interface BinaryOperations{
+	public double apply(final double firstOperand,final double secondOperand);
+}
