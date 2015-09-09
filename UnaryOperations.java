da6677ef0712f7ecb817007f48db6b55483b4412
Darr758 <Darraghk500@yahoo.ie>
2015-09-09 22:53:39 +0100

    Create UnaryOperations.java

---
+++ UnaryOperations.java
@@ -0,0 +1,9 @@
+/**
+* An interface that declares the apply method of unary operations.
+*
+* @author Darragh King (ID 113372871)
+*/
+
+public interface UnaryOperations{
+	public double apply(final double firstOperand);
+}
